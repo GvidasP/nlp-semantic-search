260b6cfe3a9986c3b88c22cc0bda909c1e1f408e
Gvidas Pranauskas <gvidaspran@gmail.com>
2021-10-05 19:48:40 +0300

    removed unused imports

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, render_template
 from flask_cors import CORS
 from dotenv import load_dotenv
 from os.path import join, dirname
