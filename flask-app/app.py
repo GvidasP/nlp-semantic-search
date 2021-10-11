546a54a10b560480181dc1d8a4c6b04854253461
Gvidas Pranauskas <gvidaspran@gmail.com>
2021-10-11 23:04:12 +0300

    added endpoint for search by index

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,13 +3,10 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 from os.path import join, dirname
 
-from universal_sentence_encoding import search_articles
+from universal_sentence_encoding import search_articles, find_similar_articles
 
 dotenv_path = join(dirname(__file__), '.env')  # Path to .env file
 load_dotenv(dotenv_path)
-
-# configuration
-DEBUG = True
 
 # instantiate the app
 app = Flask(__name__, template_folder="./dist", static_folder="./dist/static")
@@ -29,6 +26,16 @@ def search():
     search_text = request.args.get('search')
 
     articles = search_articles(search_text)
+    articles.columns = articles.columns.str.lower()
+
+    return articles.to_json(orient='index')
+
+
+@app.route('/api/ftinwsj', methods=['GET'])
+def search_similar_articles():
+    index = request.args.get('search')
+
+    articles = find_similar_articles(int(index))
     articles.columns = articles.columns.str.lower()
 
     return articles.to_json(orient='records')
--- flask-app/universal_sentence_encoding.py
+++ flask-app/universal_sentence_encoding.py
@@ -14,6 +14,9 @@ print('module loaded')
 df_articles = pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'df_articles.csv'))
 df_articles_wsj = pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'df_articles_wsj.csv'))
 
+df_articles['Abstract'] = df_articles['Tag'].astype(str) + ', ' + df_articles['Headline'].astype(str) + ', ' + df_articles['Summary'].astype(str)
+df_articles_wsj['Abstract'] = df_articles_wsj['Tag'].astype(str) + ', ' + df_articles_wsj['Headline'].astype(str) + ', ' + df_articles_wsj['Summary'].astype(str)
+
 
 def cosine(u, v):
     return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
@@ -22,7 +25,8 @@ def cosine(u, v):
 # Find articles by search_query
 def search_articles(search_query):
     search_vector = model([search_query])[0]
-    df_articles['Relevance'] = df_articles['Summary'].map(lambda x: cosine(search_vector, model([x])[0]))
+
+    df_articles['Relevance'] = df_articles['Abstract'].map(lambda x: cosine(search_vector, model([x])[0]))
 
     result = df_articles.nlargest(5, 'Relevance')
     return result
@@ -30,8 +34,8 @@ def search_articles(search_query):
 
 # Find similar article from Financial Times (index) in Wall Street Journal
 def find_similar_articles(article_index):
-    search_vector = model([df_articles.iloc[article_index]['Summary']])[0]
-    df_articles_wsj['Relevance'] = df_articles_wsj['Summary'].map(lambda x: cosine(search_vector, model([x])[0]))
+    search_vector = model([df_articles.iloc[article_index]['Abstract']])[0]
+    df_articles_wsj['Relevance'] = df_articles_wsj['Abstract'].map(lambda x: cosine(search_vector, model([x])[0]))
 
     result = df_articles_wsj.nlargest(5, 'Relevance')
     return result
